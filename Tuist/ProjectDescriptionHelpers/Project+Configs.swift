9fef2db316cc09d75ff1f87531ceec4beecf63eb
Ahmed Ramy <Dev.AhmedRamy@gmail.com>
2024-10-28 17:45:44 +0300

    fix: Tuist Configuration was hardcoding version to 1

--- Tuist/ProjectDescriptionHelpers/Project+Configs.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Configs.swift
@@ -3,6 +3,6 @@ import Foundation
 enum ProjectConfigs {
     static let bundleId = "com.automata.automata.example"
     static let version = "1.0.1"
-    static let buildNumber = "3"
+    static let buildNumber = "4"
     static let iOSLevel = "15.0"
 }
